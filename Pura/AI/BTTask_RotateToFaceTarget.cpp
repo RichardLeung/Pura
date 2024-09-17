c61dd59cef08823a3b022f42ccbff962cff4ddf0
RichardLeung <79985865@qq.com>
2024-09-17 16:14:28 +0800

    【新增】敌人攻击行为

---
+++ Pura/AI/BTTask_RotateToFaceTarget.cpp
@@ -0,0 +1,106 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_RotateToFaceTarget.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "Kismet/KismetMathLibrary.h"
+
+UBTTask_RotateToFaceTarget::UBTTask_RotateToFaceTarget()
+{
+	NodeName = "Native Rotate To Face Target Actor";
+	AnglePrecision = 10.f;
+	RotationInterpSpeed = 5.f;
+
+	bNotifyTick = true;
+	bNotifyTaskFinished = true;
+	bCreateNodeInstance = false;
+
+	INIT_TASK_NODE_NOTIFY_FLAGS();
+	
+	InTargetToFaceKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(ThisClass, InTargetToFaceKey), AActor::StaticClass());
+}
+
+void UBTTask_RotateToFaceTarget::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+	if(UBlackboardData* BBAsset = GetBlackboardAsset())
+	{
+		InTargetToFaceKey.ResolveSelectedKey(*BBAsset);
+	}
+}
+
+uint16 UBTTask_RotateToFaceTarget::GetInstanceMemorySize() const
+{
+	return sizeof(FRotateToFaceTargetMemory);
+}
+
+FString UBTTask_RotateToFaceTarget::GetStaticDescription() const
+{
+	const FString KeyDescription = InTargetToFaceKey.SelectedKeyName.ToString();
+	return FString::Printf(TEXT("Smoothly rotates to face %s key until the angle precision %s is reached"), *KeyDescription, *FString::SanitizeFloat(AnglePrecision));
+}
+
+EBTNodeResult::Type UBTTask_RotateToFaceTarget::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UObject* ActorObject = OwnerComp.GetBlackboardComponent()->GetValueAsObject(InTargetToFaceKey.SelectedKeyName);
+	AActor* TargetActor = Cast<AActor>(ActorObject);
+	APawn* OwningPawn = OwnerComp.GetAIOwner()->GetPawn();
+	FRotateToFaceTargetMemory* Memory = CastInstanceNodeMemory<FRotateToFaceTargetMemory>(NodeMemory);
+	check(Memory);
+	Memory->OwningPawn = OwningPawn;
+	Memory->TargetActor = TargetActor;
+	if(!Memory->IsValid())
+	{
+		return EBTNodeResult::Failed;
+	}
+	if (HasReachedAnglePrecision(OwningPawn, TargetActor))
+	{
+		Memory->Reset();
+		return EBTNodeResult::Succeeded;
+	}
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_RotateToFaceTarget::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	FRotateToFaceTargetMemory* Memory = CastInstanceNodeMemory<FRotateToFaceTargetMemory>(NodeMemory);
+	check(Memory);
+	if(!Memory->IsValid())
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+		return;
+	}
+	APawn* OwningPawn = Memory->OwningPawn.Get();
+	AActor* TargetActor = Memory->TargetActor.Get();
+	if (HasReachedAnglePrecision(OwningPawn, TargetActor))
+	{
+		Memory->Reset();
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	}else
+	{
+		const FRotator LookAtRot = UKismetMathLibrary::FindLookAtRotation(OwningPawn->GetActorLocation(), TargetActor->GetActorLocation());
+		const FRotator TargetRot = FMath::RInterpTo(OwningPawn->GetActorRotation(), LookAtRot, DeltaSeconds, RotationInterpSpeed);
+		OwningPawn->SetActorRotation(TargetRot);
+	}
+	
+}
+
+bool UBTTask_RotateToFaceTarget::HasReachedAnglePrecision(APawn* QueryPawn, AActor* TargetActor) const
+{
+	// Check if the angle between the owner forward vector and the vector to the target is less than the angle precision
+	// 中文：检查所有者前向矢量和指向目标的矢量之间的角度是否小于角度精度
+	const FVector OwnerForward = QueryPawn->GetActorForwardVector();
+	// Get the vector from the owner to the target and normalize it
+	// 中文：获取从所有者到目标的矢量并将其标准化
+	const FVector OwnerToTargetNormalized = (TargetActor->GetActorLocation() - QueryPawn->GetActorLocation()).GetSafeNormal();
+	// Get the dot product between the owner forward vector and the owner to target vector
+	// 中文：获取所有者前向矢量和所有者到目标矢量之间的点积
+	const float DotResult = FVector::DotProduct(OwnerForward, OwnerToTargetNormalized);
+	// Get the angle between the owner forward vector and the owner to target vector
+	// 中文：获取所有者前向矢量和所有者到目标矢量之间的角度
+	const float AngleDiff = UKismetMathLibrary::DegAcos(DotResult);
+	return AngleDiff <= AnglePrecision;
+}
