3fbf973ae6de4ddb01b46981de6e8ca767856ca6
RichardLeung <79985865@qq.com>
2024-09-30 22:50:49 +0800

    【新增】BossUI相关

--- Pura/Component/UI/EnemyUIComponent.cpp
+++ Pura/Component/UI/EnemyUIComponent.cpp
@@ -2,5 +2,26 @@
 
 
 #include "EnemyUIComponent.h"
+#include "Pura/Widget/PuraUserWidgetBase.h"
 
 
+void UEnemyUIComponent::RegisterEnemyDrawnWidget(UPuraUserWidgetBase* InWidgetToRegister)
+{
+	EnemyDrawnWidgets.Add(InWidgetToRegister);
+}
+
+void UEnemyUIComponent::RemoveEnemyDrawnWidgetsIfAny()
+{
+    if(EnemyDrawnWidgets.IsEmpty())
+    {
+        return;
+    }
+	for (UPuraUserWidgetBase* DrawnWidget : EnemyDrawnWidgets)
+    {
+        if (DrawnWidget)
+        {
+            DrawnWidget->RemoveFromParent();
+        }
+    }
+    EnemyDrawnWidgets.Empty();
+}
--- Pura/Component/UI/EnemyUIComponent.h
+++ Pura/Component/UI/EnemyUIComponent.h
@@ -6,10 +6,19 @@
 #include "PawnUIComponent.h"
 #include "EnemyUIComponent.generated.h"
 
+class UPuraUserWidgetBase;
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class PURA_API UEnemyUIComponent : public UPawnUIComponent
 {
 	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawnWidget(UPuraUserWidgetBase* InWidgetToRegister);
 
+	UFUNCTION(BlueprintCallable)
+	void RemoveEnemyDrawnWidgetsIfAny();
+
+private:
+	TArray<UPuraUserWidgetBase*> EnemyDrawnWidgets;
 };
