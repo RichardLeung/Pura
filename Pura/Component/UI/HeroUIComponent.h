96b7eba7741527d619c7e481584621bbc6f55d00
RichardLeung <79985865@qq.com>
2024-10-04 23:58:57 +0800

    【新增】特殊攻击相关UI

--- Pura/Component/UI/HeroUIComponent.h
+++ Pura/Component/UI/HeroUIComponent.h
@@ -3,12 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "PawnUIComponent.h"
 #include "Pura/Util/PuraDebugHelper.h"
 #include "HeroUIComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChangedDelegate, TSoftObjectPtr<UTexture2D>, SoftWeaponIcon);
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAbilityIconSlotUpdatedDelegate, FGameplayTag, AbilityInputTag, TSoftObjectPtr<UMaterialInterface>, SoftAbilityIconMaterial);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class PURA_API UHeroUIComponent : public UPawnUIComponent
@@ -21,4 +23,7 @@ public:
 
 	UPROPERTY(BlueprintAssignable, BlueprintCallable)
 	FOnEquippedWeaponChangedDelegate OnEquippedWeaponChanged;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnAbilityIconSlotUpdatedDelegate OnAbilityIconSlotUpdated;
 };
--- Pura/Util/PuraStructTypes.h
+++ Pura/Util/PuraStructTypes.h
@@ -30,7 +30,7 @@ struct FPuraHeroSpecialAbilitySet : public FPuraHeroAbilitySet
 	GENERATED_BODY()
 	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	TSoftObjectPtr<UMaterialInstance> AbilityIconMaterial;
+	TSoftObjectPtr<UMaterialInstance> SoftAbilityIconMaterial;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories="Player.Cooldown"))
 	FGameplayTag AbilityCooldownTag;
