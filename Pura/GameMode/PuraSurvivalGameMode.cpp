a29acb3347e449db24d1398d882a9071a2214945
RichardLeung <79985865@qq.com>
2024-10-09 12:48:14 +0800

    【新增】幸存者模式

---
+++ Pura/GameMode/PuraSurvivalGameMode.cpp
@@ -0,0 +1,64 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuraSurvivalGameMode.h"
+
+void APuraSurvivalGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	checkf(EnemyWaveSpawnerDataTable, TEXT("EnemyWaveSpawnerDataTable is not set in %s. Please set it in the editor."), *GetName());
+	SetCurrentSurvivalGameModeState(EPuraSurvivalGameModeState::WaitSpawnNewWave);
+	TotalWavesToSpawn = EnemyWaveSpawnerDataTable->GetRowNames().Num();
+}
+
+void APuraSurvivalGameMode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	if (CurrentSurvivalGameModeState == EPuraSurvivalGameModeState::WaitSpawnNewWave)
+	{
+		TimePassedSinceStart += DeltaSeconds;
+		if (TimePassedSinceStart >= SpawnNewWaveWaitTime)
+		{
+			TimePassedSinceStart = 0.f;
+			SetCurrentSurvivalGameModeState(EPuraSurvivalGameModeState::SpawningNewWave);
+		}
+	}
+	if (CurrentSurvivalGameModeState == EPuraSurvivalGameModeState::SpawningNewWave)
+	{
+		TimePassedSinceStart += DeltaSeconds;
+		if (TimePassedSinceStart >= SpawnNewWaveWaitTime)
+		{
+			// TODO: Handle Spawn New Enemy
+
+			TimePassedSinceStart = 0.f;
+			SetCurrentSurvivalGameModeState(EPuraSurvivalGameModeState::InProgress);
+		}
+	}
+	if (CurrentSurvivalGameModeState == EPuraSurvivalGameModeState::InProgress)
+	{
+		TimePassedSinceStart += DeltaSeconds;
+		if (TimePassedSinceStart >= WaveCompletedWaitTime)
+		{
+			TimePassedSinceStart = 0.f;
+			CurrentWaveCount++;
+			if (HasFinishedAllWaves())
+			{
+				SetCurrentSurvivalGameModeState(EPuraSurvivalGameModeState::AllWavesDone);
+			}else
+			{
+				SetCurrentSurvivalGameModeState(EPuraSurvivalGameModeState::WaitSpawnNewWave);
+			}
+		}
+	}
+}
+
+void APuraSurvivalGameMode::SetCurrentSurvivalGameModeState(EPuraSurvivalGameModeState InState)
+{
+	CurrentSurvivalGameModeState = InState;
+	OnSurvivalGameModeStateChanged.Broadcast(CurrentSurvivalGameModeState);
+}
+
+bool APuraSurvivalGameMode::HasFinishedAllWaves() const
+{
+	return CurrentWaveCount >= TotalWavesToSpawn;
+}
---
+++ Pura/GameMode/PuraSurvivalGameMode.h
@@ -0,0 +1,95 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PuraBaseGameMode.h"
+#include "PuraSurvivalGameMode.generated.h"
+
+class APuraEnemyCharacter;
+
+UENUM(BlueprintType)
+enum class EPuraSurvivalGameModeState: uint8
+{
+	WaitSpawnNewWave,
+	SpawningNewWave,
+	InProgress,
+	WaveCompleted,
+	AllWavesDone,
+	PlayerDied
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSurvivalGameModeStateChangedDelegate, EPuraSurvivalGameModeState, CurrentState);
+
+USTRUCT(BlueprintType)
+struct FPuraEnemyWaveSpawnerInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TSoftClassPtr<APuraEnemyCharacter> SoftEnemyClassToSpawn;
+
+	UPROPERTY(EditAnywhere)
+	int32 MinPerSpawnCount = 1;
+
+	UPROPERTY(EditAnywhere)
+	int32 MaxPerSpawnCount = 3;
+};
+
+USTRUCT(BlueprintType)
+struct FPuraEnemyWaveSpawnerTableRow: public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TArray<FPuraEnemyWaveSpawnerInfo> EnemyWaveSpawnerInfos;
+
+	UPROPERTY(EditAnywhere)
+	int32 TotalEnemyToSpanThisWave = 1;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class PURA_API APuraSurvivalGameMode : public APuraBaseGameMode
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+	void SetCurrentSurvivalGameModeState(EPuraSurvivalGameModeState InState);
+
+	bool HasFinishedAllWaves() const;
+	
+	UPROPERTY()
+	EPuraSurvivalGameModeState CurrentSurvivalGameModeState;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnSurvivalGameModeStateChangedDelegate OnSurvivalGameModeStateChanged;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enemy Wave Spawner", meta = (AllowPrivateAccess = "true"))
+	UDataTable* EnemyWaveSpawnerDataTable;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Enemy Wave Spawner", meta = (AllowPrivateAccess = "true"))
+	int32 TotalWavesToSpawn;
+
+	UPROPERTY()
+	int32 CurrentWaveCount = 1;
+
+	UPROPERTY()
+	float TimePassedSinceStart = 0.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enemy Wave Spawner", meta = (AllowPrivateAccess = "true"))
+	float SpawnNewWaveWaitTime = 5.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enemy Wave Spawner", meta = (AllowPrivateAccess = "true"))
+	float SpawnEnemyDelayTime = 2.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enemy Wave Spawner", meta = (AllowPrivateAccess = "true"))
+	float WaveCompletedWaitTime = 5.f;
+};
